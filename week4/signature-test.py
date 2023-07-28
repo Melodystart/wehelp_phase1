c7e78d650cc739b9503f28f565d0d523c0bef748
Melodystart <start99start@gmail.com>
2023-07-28 11:34:54 +0800

    W4:add verify-signature-test

--- week4/signature-test.py
+++ week4/signature-test.py
@@ -10,18 +10,21 @@ timestamp = base64_encode(time.to_bytes(4, byteorder='big'))
 
 msg = session_value + (b'.') + timestamp
 
-#方法一：有加salt
-s = Signer(secret_key, salt='cookie-session', sep='.', key_derivation='hmac', digest_method=sha1, algorithm=None)
-ans1 = base64_encode(s.get_signature(msg))
-
-#方法二：
-h = hmac.new(secret_key, msg, sha1)
-ans2 = base64_encode(h.hexdigest())
-
-#網頁cookie: eyJTSUdORUQtSU4iOnRydWV9.ZMCQ0g.mQ06_NzwO_qHmHyMrFj-j9ga3ME
 print(session_value.decode("utf-8")) #session相符:eyJTSUdORUQtSU4iOnRydWV9
 print(timestamp.decode("utf-8"))     #timestamp相符:ZMCQ0g
-#方法一不符:QXBWUm85cXBWWUppZ21LUGVjZHNzM095Z1BJ
+
+#方法一簽章：(加salt)
+s = Signer(secret_key, salt='cookie-session', sep='.', key_derivation='hmac', digest_method=sha1, algorithm=None)
+ans1 = base64_encode(s.get_signature(msg))
 print(ans1.decode("utf-8"))
-#方法二不符:ZmRkZGJmMjZhNTM0ZDVhNmQwMTEyNDkwNjgwMGE5ODY2ODEzNjcxOQ
-print(ans2.decode("utf-8"))  
+#方法一驗證簽章是否正確：
+deans1 = base64_decode(ans1)
+print(s.verify_signature(msg,deans1))
+
+#方法二簽章：
+h = hmac.new(secret_key, msg, sha1)
+ans2 = base64_encode(h.hexdigest())
+print(ans2.decode("utf-8"))
+#方法二驗證簽章是否正確：
+deans2 = base64_decode(ans2)
+print(hmac.compare_digest(bytes(h.hexdigest(), 'utf-8'), deans2))
